fee05e37407e92883b4c63c6e6c180a952e47824
fardeenKhadri <fardeeinshakhadrii@gmail.com>
2025-02-20 21:17:47 +0530

    New Name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pysummarizer",
+    name="pysuma",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
